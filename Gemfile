6766da1146bda0c1053a64b166600b0861314e53
zakusenSS <cycpp866@yahoo.co.jp>
2025-05-13 15:35:09 +0900

    モデルの単体テストを実装

--- Gemfile
+++ Gemfile
@@ -47,6 +47,9 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+  gem 'rspec-rails', '~> 4.0.0'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 group :development do
@@ -58,7 +61,7 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
-
+  gem 'rubocop', '1.71.2', require: false
 end
 
 group :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,18 @@ class User < ApplicationRecord
   has_many :comments
   has_many :orders
 
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+    
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'には全角カタカナを使用してください' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'には全角カタカナを使用してください' }
+    validates :email
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/ }
     validates :birthday
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-    validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
   end
 
 end

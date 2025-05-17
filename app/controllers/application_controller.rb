06eaf88c77ea7436b974e3119b445e36a3f62791
zakusenSS <cycpp866@yahoo.co.jp>
2025-05-17 15:44:02 +0900

    ActiveStorageとActiceHash導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,16 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
+
+
+
+  
   private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday ])
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # 商品テーブルのアソシエーション
+  belongs_to :user
+  has_one    :purchases
+
+  # ActiveSStorageのアソシエーション
+  has_one_attached :image
+
+  # ActiveHashのアソシエーション
+  belongs_to :category
+  belongs_to :status_id
+  belongs_to :shipping_fee_id
+  belongs_to :prefecture_id
+  belongs_to :delivery_time_id
+
+
+
+
 end

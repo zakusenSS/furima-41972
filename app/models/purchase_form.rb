009a9ce12f71caec1d8d94e93033040777fba499
zakusenSS <cycpp866@yahoo.co.jp>
2025-06-03 22:43:29 +0900

    Formオブジェクトを作成する

---
+++ app/models/purchase_form.rb
@@ -0,0 +1,29 @@
+class purchaseForm
+  include ActiveModel: :Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number , format: { with: /\A[0-9]{10,11}\z/, message: 'must be 10 or 11 digits longs' }
+  end
+
+  def save
+    purchase = purchase.create(user_id:, item_id:)
+
+    shippingAddress.create(
+      postal_code:,
+      prefecture_id:,
+      city:,
+      address:,
+      building_name:,
+      phone_number:,
+      purchase_id: purchase.id
+    )
+  end
+end
+
